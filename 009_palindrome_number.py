521a1ea426b1b11f4923fa4f80216edae513db40
xrshih <stanley38566@gmail.com>
2026-08-30 08:32:44 +0800

    feat: solve 009 Palindrome Number

---
+++ 009_palindrome_number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        s = str(x)
+        left = 0
+        right= len(s) -1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
